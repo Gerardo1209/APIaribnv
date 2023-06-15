df63a5505899c0141c25c1775144d6c0f7cb2853
Gerardo12002 <fematdelgado.github@gmail.com>
2023-06-14 23:34:56 -0600

    API para airbnv

---
+++ functions/index.js
@@ -0,0 +1,42 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+const express = require("express"); //importar express
+const bodyParser = require("body-parser");
+const misRutas = require("./routes/rutas");
+const app = express(); //crear al servidor
+const cors = require("cors");
+const port = 3001;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/", misRutas);
+
+app.listen(port, (err, res) => {
+  if (err) {
+    logger.info("Error al levantar servidor");
+    return;
+  }
+  logger.info("Apis escuchando en el puerto " + port);
+});
+
+//module.exports = this.app;
+exports.api = onRequest(app);
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
---
+++ functions/routes/rutas.js
@@ -0,0 +1,155 @@
+const express = require("express"); // Importar express
+const router = express.Router();
+const nodemailer = require("nodemailer");
+const { body, param, validationResult } = require("express-validator");
+
+router.post(
+  "/reserva",
+  [
+    body("casa").not().isEmpty(),
+    body("usr").not().isEmpty(),
+    body("fechaInicio").not().isEmpty(),
+    body("fechaFinal").not().isEmpty()
+  ],
+  (req, res) => {
+    console.log("estoy en el API post")
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({ success: false, err: JSON.stringify(errors) });
+      return;
+    }
+    let body = req.body;
+    sendMail(body);
+  }
+);
+router.post(
+  "/contacto",
+  [
+    body("desarrollo").not().isEmpty(),
+    body("usr").not().isEmpty()
+  ],
+  (req, res) => {
+    console.log("estoy en el API post")
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({ success: false, err: JSON.stringify(errors) });
+      return;
+    }
+    let body = req.body;
+    sendMail2(body);
+  }
+);
+async function sendMail(data) {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp-mail.outlook.com", // hostname
+        secureConnection: false, // use SSL
+        port: 587, // port for secure SMTP
+        auth: {
+            user: "al289539@edu.uaa.mx",
+            pass: "Pakwar10!"
+        }
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"AirBnV" <al289539@edu.uaa.mx>', // sender address
+      to: data.usr.email, // list of receivers
+      subject: "Reserve de casa en AirBnV", // Subject line
+      text: "Gracias por su preferencia", // plain text body
+      html: `
+            <h2>Se ha guardado exitosamente su registro ${data.usr.displayName}</h2>
+            <br>
+            <b><span>su reservacion de ${data.casa.nombre}, por el precio de ${data.casa.precio}</span></b>
+            <br>
+            <b><span>entre los días ${data.fechaInicio  } y ${data.fechaFinal} </b>
+            
+            `, // html body
+    });
+  
+    async function sendMail2(data) {
+      // Generate test SMTP service account from ethereal.email
+      // Only needed if you don't have a real mail account for testing
+      let testAccount = await nodemailer.createTestAccount();
+    
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+          host: "smtp-mail.outlook.com", // hostname
+          secureConnection: false, // use SSL
+          port: 587, // port for secure SMTP
+          auth: {
+              user: "al289539@edu.uaa.mx",
+              pass: "Pakwar10!"
+          }
+      });
+    
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"AirBnV" <al289539@edu.uaa.mx>', // sender address
+        to: data.usr.email, // list of receivers
+        subject: "Gracias por ponerse en contacto con nosotros", // Subject line
+        text: "Gracias por su preferencia", // plain text body
+        html: `
+              <h2>Se recibió su mensaje, en la brevedad nos pondremos en contacto con usted</h2>
+              <br>
+              <b>${data.desarrollo}</span></b>
+              <br>
+              
+              `, // html body
+      });
+    
+  }
+}
+
+router.get("/inversiones", (req, res) => {
+  res.send({
+    title: "Invercionistas",
+    active: true,
+    members: [
+      {
+        name: "Elon Musk",
+        age: 55,
+        investment: 420000000,
+        happiness: "¡Estoy muy feliz de invertir en esta oportunidad!",
+      },
+      {
+        name: "Warren Buffett",
+        age: 91,
+        investment: 800000000,
+        happiness: "Invertir siempre me llena de alegría y emoción.",
+      },
+      {
+        name: "Jeff Bezos",
+        age: 58,
+        investment: 650000000,
+        happiness: "Es un honor formar parte de esta inversión exitosa.",
+      },
+      {
+        name: "Sara Blakely",
+        age: 50,
+        investment: 30000000,
+        happiness: "Estoy encantada de apoyar este proyecto innovador.",
+      },
+      {
+        name: "Carlos Slim",
+        age: 81,
+        investment: 550000000,
+        happiness:
+          "Mi inversión es un reflejo de mi confianza en el potencial de esta empresa.",
+      },
+      {
+        name: "Cathie Wood",
+        age: 66,
+        investment: 150000000,
+        happiness:
+          "Invertir en el futuro siempre me llena de felicidad y entusiasmo.",
+      },
+    ],
+  });
+});
+
+module.exports = router;
